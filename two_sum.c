1b2cdab34bd4ca29b6fbc03766db557989adac36
tingyawang <tingya0218@gmail.com>
2022-10-11 20:00:23 +0800

    Create two_sum.c
    Added two sum function of leetcode (Eazy)

---
+++ two_sum.c
@@ -0,0 +1,20 @@
+**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+    int i, j;
+    for(i = 0; i < numsSize; i++){
+        for(j = (i + 1); j < numsSize; j++){
+            if(target == *(nums + i) + *(nums + j)){
+                memset(nums , 0, sizeof(int) * numsSize);
+                *nums = i;
+                *(nums + 1) = j;
+                *returnSize = 2;
+                return nums;
+            }
+        }
+    }
+    return nums;
+    
+}
