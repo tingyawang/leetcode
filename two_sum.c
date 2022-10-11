4cfcc4f9efd31391e78bd4edd5c37246e529623f
tingyawang <tingya0218@gmail.com>
2022-10-12 01:04:12 +0800

    Add files via upload
    dded two sum example

--- two_sum.c
+++ two_sum.c
@@ -1,13 +1,14 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
-
+#include <stdio.h>
+#include <stdlib.h>
 int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     int i, j;
     for(i = 0; i < numsSize; i++){
         for(j = (i + 1); j < numsSize; j++){
             if(target == *(nums + i) + *(nums + j)){
-                memset(nums , 0, sizeof(int) * numsSize);
+                //memset(nums, 0,sizeof(int) * numsSize);
                 *nums = i;
                 *(nums + 1) = j;
                 *returnSize = 2;
@@ -17,4 +18,16 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize){
     }
     return nums;
     
+}
+
+void main()
+{
+	int nums[4] = {0,2,4,0}, target = 0;
+	int i, numsSize = 3, returnSize = 2;
+    
+    twoSum(&nums[0], numsSize, target, &returnSize);
+/*	for(i= 0; i < returnSize; i++){
+    	    printf("output: [%d] \r\n", nums[i]);
+    }
+*/
 }
